1664b0d70013ab4a9663102499fe4d5dbac93b70
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-08-03 18:48:39 +0530

    modified props

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -12,15 +12,15 @@ function App() {
       <div className="flex justify-center space-x-8 mt-12">
         <div className="w-1/3 p-6 bg-blue-200 rounded-lg shadow-lg">
           <h2 className="text-white text-xl mb-4 text-center bg-blue-800 py-2 rounded">Pending</h2>
-          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S" val="0" />
-          <TaskCard title="Add blog" dueDate="22nd March" assigneeName="Rohit M" val="0" />
+          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S"/>
+          <TaskCard title="Add blog" dueDate="22nd March" assigneeName="Rohit M"/>
           <p className="text-black">&#x2795; New task</p>
         </div>
 
         <div className="w-1/3 p-6 bg-blue-200 rounded-lg shadow-lg">
           <h2 className="text-white text-xl mb-4 text-center bg-blue-800 py-2 rounded">Done</h2>
-          <TaskCard title="Design the mockup" completedAtDate="10th April" assigneeName="Rohit M" val="1" />
-          <TaskCard title="Get approval from principal" completedAtDate="20th April" assigneeName="Ajay S" val="1" />
+          <TaskCard title="Design the mockup" completedAtDate="10th April" assigneeName="Rohit M"/>
+          <TaskCard title="Get approval from principal" completedAtDate="20th April" assigneeName="Ajay S"/>
         </div>
       </div>
     </div>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,24 +1,28 @@
-import './TaskCard.css';
 import React from 'react';
+import './TaskCard.css';
 
 interface TaskCardProps {
   title: string;
   dueDate?: string;
   completedAtDate?: string;
   assigneeName: string;
-  val: '0' | '1';
 }
 
+const isValidDate = (dateString?: string): boolean => {
+  if (!dateString) return false;
+  return true;
+};
+
 const TaskCard: React.FC<TaskCardProps> = (props) => {
-  const { val, title, dueDate, completedAtDate, assigneeName } = props;
-  const isPendingTask = val === '0';
+  const { title, dueDate, completedAtDate, assigneeName } = props;
 
   return (
     <div className="TaskItem bg-cyan-100">
       <h2 className="text-black text-xl font-bold">{title}</h2>
-      {isPendingTask ? (
+      {isValidDate(dueDate) && (
         <p className="text-black text-xl font-bold">Due on: {dueDate}</p>
-      ) : (
+      )}
+      {isValidDate(completedAtDate) && (
         <p className="text-black text-xl font-bold">Completed on: {completedAtDate}</p>
       )}
       <p className="text-black text-xl font-bold">Assignee: {assigneeName}</p>
