9400c2a7a9fc4583e55681053f7dbf6c8f0dc135
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-08-06 18:22:07 +0530

    removed output directory

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-    build: {
-      outDir: 'build',
-    },
     plugins: [react()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
