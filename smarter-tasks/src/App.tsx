58ae61e3d8304d575b01aca48290add2cabe0cec
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-08-06 18:43:59 +0530

    fixed errors

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import TaskApp from "./TaskApp";
 
 function App() {
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Task from "./Task";
-import { TaskItem } from "./types";
 
 interface Props {
   tasks: TaskItem[];
