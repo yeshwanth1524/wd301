71d3221c48d9e06a9b58882e9038ed9f23b7e30a
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-08-27 18:35:47 +0530

    minor changes

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -54,6 +54,7 @@ return (
           onChange={titleChanged}
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
+          required
         />
         <label
           htmlFor="todoTitle"
@@ -101,6 +102,7 @@ return (
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          id='addTaskButton'
         >
           Add item
         </button>
