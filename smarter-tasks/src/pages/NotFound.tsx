1fcc5674641b056a03e2e6d22ce6fd2de1617cb9
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-09-06 18:51:30 +0530

    minor changes

--- smarter-tasks/src/pages/NotFound.tsx
+++ smarter-tasks/src/pages/NotFound.tsx
@@ -1,11 +1,12 @@
 function NotFound() {
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-            <h1 className="text-red-600 text-6xl font-bold mb-4">404 - Page Not Found</h1>
+            <h1 className="text-red-600 text-6xl font-bold mb-6">404 - Page Not Found</h1>
             <a href="/home" >
-                <button className="px-4 py-2 bg-blue-600 hover:bg-blue-800 rounded text-black" id="backToHomeButton">Back to Home</button>
+                <button className="px-4 py-2 bg-blue-600 hover:bg-blue-800 rounded text-white font-bold" id="backToHomeButton">Back to Home</button>
             </a>
         </div>
     )
 }
+
 export default NotFound;
