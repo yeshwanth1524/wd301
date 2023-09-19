255f2e930ae1d8320186358ab993b9acae2946d3
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-09-19 18:10:20 +0530

    level 6

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,49 +1,41 @@
-import { createBrowserRouter, Navigate , RouterProvider,} from "react-router-dom";
-import HomePage from './pages/HomePage';
-import TaskListPage from './pages/TaskListPage';
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Layout from "./Layout";
-import Signin from "./pages/Signin";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Notfound from "./pages/Notfound";
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import Dashboard from "./pages/dashboard";
 import ProtectedRoute from "./ProtectedRoute";
-import NotFound from "./pages/Notfound";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signin" replace />,
+    element: <Signup />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
   {
     path: "/signin",
     element: <Signin />,
   },
   {
+    path: "/notfound",
+    element: <Notfound />,
+  },
+  {
+    path: "/dashboard",
     element: (
       <ProtectedRoute>
-        <Layout />
+        <Dashboard />
       </ProtectedRoute>
     ),
-    children: [
-      {
-        path: "home",
-        element: (<HomePage />)
-      },
-      {
-        path: "tasks",
-        element: (<TaskListPage />)
-      },
-      {
-        path: "tasks/:id",
-        element: (<TaskDetailsPage />)
-      },
-      {
-        path: "*",
-        element: <Navigate to="/notfound" replace />
-      },
-      {
-        path: "Notfound",
-        element: <NotFound />,
-      }  
-    ]
+  },
+  {
+    path: "*",
+    element: <Notfound />,
   }
 ]);
 
@@ -53,4 +45,67 @@ const App = () => {
   );
 }
 
-export default App
+export default App;
+
+// import { createBrowserRouter, Navigate , RouterProvider,} from "react-router-dom";
+// import HomePage from './pages/HomePage';
+// import TaskListPage from './pages/TaskListPage';
+// import TaskDetailsPage from "./pages/TaskDetailsPage";
+// import Layout from "./Layout";
+// import Signin from "./pages/Signin";
+// import ProtectedRoute from "./ProtectedRoute";
+// import NotFound from "./pages/Notfound";
+// import ReactPlayground from './ReactPlayground';
+// import Form from './Form';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Navigate to="/signin" replace />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <Signin />,
+//   },
+//   {
+//     element: (
+//       <ProtectedRoute>
+//         <Layout />
+//       </ProtectedRoute>
+//     ),
+//     children: [
+//       {
+//         path: "home",
+//         element: (<HomePage />)
+//       },
+//       {
+//         path: "tasks",
+//         element: (<TaskListPage />)
+//       },
+//       {
+//         path: "tasks/:id",
+//         element: (<TaskDetailsPage />)
+//       },
+//       {
+//         path: "*",
+//         element: <Navigate to="/notfound" replace />
+//       },
+//       {
+//         path: "Notfound",
+//         element: <NotFound />,
+//       }  
+//     ]
+//   }
+// ]);
+
+// const App = () => {
+//   return (
+//     <>
+//       <Form />
+//       <ReactPlayground />
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// }
+
+// export default App
--- smarter-tasks/src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface TaskItem {
-  id: string;
-  title: string;
-  dueDate: string;
-  description: string;
-}
