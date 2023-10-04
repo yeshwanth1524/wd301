97cbf1f2479b41b3fb3468782602e9a20444d91d
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-10-05 00:10:23 +0530

    level 9

--- smarter-tasks/src/pages/projects/index.tsx
+++ smarter-tasks/src/pages/projects/index.tsx
@@ -1,15 +1,25 @@
-import ProjectList from "./ProjectList";
+import React, { Suspense } from "react";
+const ProjectList = React.lazy(() => import("./ProjectList"));
 import NewProject from "./NewProject";
+import ErrorBoundary from "../../components/ErrorBoundary";
 
 const Projects = () => {
   return (
     <>
       <div className="flex justify-between">
-        <h2 className="text-2xl font-medium tracking-tight">Projects</h2>
+        <h2 className="text-2xl font-medium tracking-tight text-slate-700">
+          Projects
+        </h2>
         <NewProject />
       </div>
-      <ProjectList />
+      <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <ProjectList />
+        </Suspense>
+      </ErrorBoundary>
     </>
-  )
-}
+  );
+};
+
+
 export default Projects;
--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -1,12 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      "process.env.NODE_ENV": `"${mode}"`,
-    },
-  });
-}; 
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true // For making sure that the PWA is testable from the Local dev environment
+      },
+      manifest: {
+        name: "Smarter Tasks application",
+        short_name: "Smarter Tasks",
+        icons: [
+          {
+            "src": "/favicon.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "/favicon-16x16.png",
+            "type": "image/png",
+            "sizes": "16x16"
+          },
+          {
+            "src": "/favicon-32x32.png",
+            "type": "image/png",
+            "sizes": "32x32"
+          },
+          {
+            "src": "/android-launchericon-192-192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "/android-launchericon-512-512.png",
+            "type": "image/png",
+            "sizes": "512x512",
+            "purpose": "any maskable" // Icon format that ensures that your PWA icon looks great on all Android devices
+          }
+        ],
+        theme_color: '#AAF',
+      },
+    }),
+  ],
+});
