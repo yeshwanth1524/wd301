e56701f7b1a1ec30b5c72209299ce778edd4cd59
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-10-08 00:06:11 +0530

    minor change

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -6,10 +6,10 @@ import { VitePWA } from "vite-plugin-pwa"
 
 export default ({ mode }) => {
   return defineConfig({
-  build : {
-    outDir : "dev-dist",
-  },
-  plugins: [
+    build: {
+      outDir: "dev-dist",
+    },
+   plugins: [
     react(),
     VitePWA({
       devOptions: {
@@ -21,7 +21,7 @@ export default ({ mode }) => {
         icons: [
           {
             "src": "/favicon.ico",
-            "sizes": "16x16",
+            "sizes": "64x64 32x32 24x24 16x16",
             "type": "image/x-icon"
           },
           {
@@ -42,15 +42,16 @@ export default ({ mode }) => {
           {
             "src": "/pwa-512x512.png",
             "type": "image/png",
-            "sizes": "512x512"
+            "sizes": "512x512",
+            "purpose": "any maskable" // Icon format that ensures that your PWA icon looks great on all Android devices
           }
         ],
         theme_color: '#AAF',
       },
     }),
   ],
-  define: {
-    "process.env.NODE_ENV": `"${mode}"`,
-  },
-});
+    define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+  });
 };
